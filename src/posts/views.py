ba698c8a81879a520f9caca4a8c10c10e6cb4504
Alejandro Hernández <hnzalejandro@gmail.com>
2018-05-16 16:00:09 -0400

    Añadiendo vistas

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,5 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def post_home(request):
-    return HttpResponse("<h1>Hola, mundo!</h1>")
+def post_create(request):
+    return HttpResponse("<h1>Create post</h1>")
+
+def post_detail(request):
+    return HttpResponse("<h1>Post detail</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete post</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Update post</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>Posts lists</h1>")
